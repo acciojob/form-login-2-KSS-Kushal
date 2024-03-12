893ee7dc1d11fa5534da80506c4c7f647f1a6e55
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-12 22:17:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function myFunc() {
+	const firstName = document.getElementById('fname').value;
+	const LastName = document.getElementById('lname').value;
+	const phone = document.getElementById('phone').value;
+	const email = document.getElementById('email').value;
+	const ans = `First Name: ${firstName} Last Name: ${LastName} Phone Number: ${phone} Email ID: ${email}`
+	console.log(ans)
+	alert(ans);
+}
